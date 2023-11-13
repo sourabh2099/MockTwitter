71186f5c3c0f4f4df22c94ce6f391c21b56b7a76
sourabh2099 <sourabhchowdhury82@gmail.com>
2023-11-13 17:07:15 +0530

    adding config server

--- config-server/src/main/java/com/twitter/microservice/config/server/ConfigServerApplication.java
+++ config-server/src/main/java/com/twitter/microservice/config/server/ConfigServerApplication.java
@@ -1,2 +1,20 @@
-package com.twitter.microservice.config.server;public class ConfigServerApplication {
+package com.twitter.microservice.config.server;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+import java.time.LocalDateTime;
+
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigServerApplication {
+    private static final Logger LOG = LoggerFactory.getLogger(ConfigServerApplication.class);
+    public static void main(String[] args) {
+        LOG.info("Starting Config Server App at {}", LocalDateTime.now());
+        SpringApplication.run(ConfigServerApplication.class, args);
+        LOG.info("Started Config Server App at {}", LocalDateTime.now());
+    }
 }
--- kafka-to-elastic/src/main/java/com/twitter/microservice/kafka/to/elastic/KafkaToElasticServiceApplication.java
+++ kafka-to-elastic/src/main/java/com/twitter/microservice/kafka/to/elastic/KafkaToElasticServiceApplication.java
@@ -1,2 +1,19 @@
-package com.twitter.microservice.kafka.to.elastic;public class KafkaToElasticServiceApplication {
+package com.twitter.microservice.kafka.to.elastic;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.time.LocalDateTime;
+
+@SpringBootApplication
+public class KafkaToElasticServiceApplication {
+    private static final Logger LOG = LoggerFactory.getLogger(KafkaToElasticServiceApplication.class);
+
+    public static void main(String[] args) {
+        LOG.info("Starting Kafka-to-elatic-service at {}", LocalDateTime.now());
+        SpringApplication.run(KafkaToElasticServiceApplication.class, args);
+        LOG.info("Stated Kafka-to-elastic-service at {}", LocalDateTime.now());
+    }
 }
