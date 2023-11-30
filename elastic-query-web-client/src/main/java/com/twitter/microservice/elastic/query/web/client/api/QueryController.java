e068dee85f3f8f4d5cab8c8d1effa14daf15593d
sourabh2099 <sourabhchowdhury82@gmail.com>
2023-11-30 12:05:10 +0530

    creating oauth2 branch

--- elastic-query-web-client/src/main/java/com/twitter/microservice/elastic/query/web/client/api/QueryController.java
+++ elastic-query-web-client/src/main/java/com/twitter/microservice/elastic/query/web/client/api/QueryController.java
@@ -14,6 +14,7 @@ import java.util.List;
 
 @Controller
 public class QueryController {
+    // dummy commit
     private static final Logger LOG = LoggerFactory.getLogger(QueryController.class);
     private final ElasticQueryWebClient elasticQueryWebClient;
 
